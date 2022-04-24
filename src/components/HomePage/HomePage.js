8ecfbfbfa2c624ce174b92032c48f3266c360820
Mark-Nelovka <87484591+Mark-Nelovka@users.noreply.github.com>
2022-04-24 18:22:32 +0300

    Change style

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,24 +1,44 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import axios from 'axios';
+import s from './HomePage.module.css';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const KEY = '35d12bb42e646b3da19939eafb14620b';
 
 function HomePage() {
   const [homeMovies, setHomeMovies] = useState([]);
-  
+
   useEffect(() => {
-    axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${KEY}`).then((res) => {
-      setHomeMovies(res.data.results)
-    });
-  },[])
-    return (
-        <ul>
-      {homeMovies.map((data) => {
-        return <Link key={data.id} to={`movies/${data.id}`}><li>{data.title || data.original_name}</li></Link>
-      })}
-    </ul>
-    )
+    axios
+      .get(`https://api.themoviedb.org/3/trending/all/day?api_key=${KEY}`)
+      .then(res => {
+        console.log('🚀 ~ file: HomePage.js ~ line 15 ~ useEffect ~ res', res);
+        setHomeMovies(res.data.results);
+      });
+  }, []);
+  return (
+    <>
+      <h1 className={s.title}>Top 20 films</h1>
+      <ul className={s.movieList}>
+        {homeMovies.map(data => {
+          return (
+            <li key={data.id} className={s.movieItem}>
+              <Link className={s.item} to={`movies/${data.id}`}>
+                {data.title || data.original_name}
+              </Link>
+              <p>
+                Release Date:
+                <span>
+                  {' '}
+                  {data.release_date ? data.release_date : 'No date'}
+                </span>
+              </p>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
 
 export default HomePage;
--- src/components/Navigations/Navigation.js
+++ src/components/Navigations/Navigation.js
@@ -2,20 +2,30 @@ import s from './navigation.module.css';
 import { NavLink } from 'react-router-dom';
 
 function Navigation() {
-    return (
-        <header className={s.header}>
-            <nav>
-            
-                <NavLink to="/"  className={({isActive}) =>`${isActive ? s.active : s.link}`}>
-                Home
-                </NavLink> 
-            
-            <NavLink to="/movies" className={({isActive}) =>`${isActive ? s.active : s.link}`}>
-                Movies    
+  return (
+    <header className={s.header}>
+      <nav>
+        <ul className={s.navList}>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => `${isActive ? s.active : s.link}`}
+            >
+              Home
             </NavLink>
-        </nav>
-        </header>
-    )
+          </li>
+          <li>
+            <NavLink
+              to="/movies"
+              className={({ isActive }) => `${isActive ? s.active : s.link}`}
+            >
+              Movies
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Navigation;
